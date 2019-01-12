fa6a84d321ef5b86db80d5f354dd88fe6fa6ecca
noobpersonTOs <46160387+noobpersonTOs@users.noreply.github.com>
2019-01-12 01:53:11 -0800

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,89 @@
+import discord
+from discord.ext import commands
+import asyncio
+import requests, bs4
+import os
+import time
+import youtube_dl
+from discord import opus
+
+client = commands.Bot(command_prefix=("x"))
+client.remove_command("help")
+
+players = {}	
+
+@client.event 
+async def on_ready():
+	print('Logged in as')
+	print("User name:", client.user.name)
+	print("User id:", client.user.id)
+	print('---------------')
+
+@client.event
+async def on_message(message):
+  if message.content == 'm.stop':
+      serverid = message.server.id
+      players[serverid].stop()
+      await client.send_message(message.channel, "Player stopped")
+  if message.content == 'm.pause':
+      serverid = message.server.id
+      players[serverid].pause()
+      await client.send_message(message.channel, "Player paused")
+  if message.content == 'm.resume':
+      serverid = message.server.id
+      players[serverid].resume()
+      await client.send_message(message.channel, "Player resumed")
+  if message.content.startswith('m.play '):
+      author = message.author
+      name = message.content.replace("m.play ", '')                 
+      fullcontent = ('http://www.youtube.com/results?search_query=' + name)
+      text = requests.get(fullcontent).text
+      soup = bs4.BeautifulSoup(text, 'html.parser')
+      img = soup.find_all('img')
+      div = [ d for d in soup.find_all('div') if d.has_attr('class') and 'yt-lockup-dismissable' in d['class']]
+      a = [ x for x in div[0].find_all('a') if x.has_attr('title') ]
+      title = (a[0]['title'])
+      a0 = [ x for x in div[0].find_all('a') if x.has_attr('title') ][0]
+      url = ('http://www.youtube.com'+a0['href'])
+      delmsg = await client.send_message(message.channel, 'Now Playing ** >> ' + title + '**')
+      server = message.server
+      voice_client = client.voice_client_in(server)
+      player = await voice_client.create_ytdl_player(url)
+      players[server.id] = player
+      print("User: {} From Server: {} is playing {}".format(author, server, title))
+      player.start()
+  await client.process_commands(message)
+
+@client.command(pass_context=True, no_pm=True)
+async def ping(ctx):
+    pingtime = time.time()
+    pingms = await client.say("Pinging...")
+    ping = (time.time() - pingtime) * 1000
+    await client.edit_message(pingms, "Pong! :ping_pong: ping time is `%dms`" % ping)
+    
+@client.command(pass_context=True, no_pm=True)
+async def join(ctx):
+    channel = ctx.message.author.voice.voice_channel
+    await client.join_voice_channel(channel)
+    await client.say('Connected to voice channel: **[' + str(channel) + ']**')
+
+@client.command(pass_context=True, no_pm=True)
+async def leave(ctx):
+    server = ctx.message.server
+    channel = ctx.message.author.voice.voice_channel
+    voice_client = client.voice_client_in(server)
+    await voice_client.disconnect()
+    await client.say("Successfully disconnected from ***[{}]***".format(channel))
+
+@client.command(pass_context=True, no_pm=True)
+async def help(ctx):
+	embed = discord.Embed(title="Help section", description=" ", color=0xFFFF)
+	embed.add_field(name="xjoin", value="make the bot join voice channel")
+	embed.add_field(name="xleave", value="make the bot leave the voice channel")
+	embed.add_field(name="xplay", value="please be careful when using this command it will break if theres music playing.")
+	embed.add_field(name="xstop", value="to stop the music from playing")
+	embed.add_field(name="xping", value="get bot's ping time")
+	await client.say(embed=embed)
+
+client.loop.create_task(change_status())
+client.run(os.environ['BOT_TOKEN'])
