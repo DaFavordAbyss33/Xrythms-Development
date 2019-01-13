3e2aa97c7f29e1545fb3176a4562fa62016cd2a0
DaFavordAbyss33 <43502572+DaFavordAbyss33@users.noreply.github.com>
2019-01-13 13:17:17 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,7 +9,7 @@ import time
 import youtube_dl
 from discord import opus
 
-client = commands.Bot(command_prefix=("XR|"))
+client = commands.Bot(command_prefix=("x"))
 client.remove_command("help")
 status = ["Still Undergoing Development Bare With :)", "xhelp For Commands :)", "Any issues dm A.price#9746"]
 
