a99966c2323369072becf5d96dc0e1d2e01f1fdd
noobpersonTOs <46160387+noobpersonTOs@users.noreply.github.com>
2019-01-31 22:53:42 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,10 +9,19 @@ import inspect
 import datetime
 from discord import opus
 
-client = commands.Bot(command_prefix=("x"))
+start_time = time.time()
+
+client = commands.Bot(command_prefix=("m."))
 client.remove_command("help")
 
-players = {}	
+players = {}
+queues = {}
+
+def check_queue(id):
+	if queues[id] != []:
+		player = queues[id].pop(0)
+		players[id] = player
+		player.start()
 
 @client.event 
 async def on_ready():
@@ -21,50 +30,55 @@ async def on_ready():
 	print("User id:", client.user.id)
 	print('---------------')
 
-def user_is_me(ctx):
-	return ctx.message.author.id == "381562121865003009"
-
 @client.command(pass_context=True, no_pm=True)
 async def ping(ctx):
     pingtime = time.time()
     pingms = await client.say("Pinging...")
     ping = (time.time() - pingtime) * 1000
     await client.edit_message(pingms, "Pong! :ping_pong: ping time is `%dms`" % ping)
-	
-@client.command(name='eval', pass_context=True)
-@commands.check(user_is_me)
-async def _eval(ctx, *, command):
-    res = eval(command)
-    if inspect.isawaitable(res):
-        await client.say(await res)
-    else:
-        await client.delete_message(ctx.message)
-        await client.say(res)
-
-@_eval.error
-async def eval_error(error, ctx):
-	if isinstance(error, discord.ext.commands.errors.CheckFailure):
-		text = "Sorry {} You can't use this command only the bot owner can do this.".format(ctx.message.author.mention)
-		await client.send_message(ctx.message.channel, text)
     
-@client.command(pass_context=True, no_pm=True)
-async def join(ctx):
+@client.command(name="join", pass_context=True, no_pm=True)
+async def _join(ctx):
+    user = ctx.message.author
     channel = ctx.message.author.voice.voice_channel
     await client.join_voice_channel(channel)
-    await client.say('Connected to voice channel: **[' + str(channel) + ']**')
+    embed = discord.Embed(colour=user.colour)
+    embed.add_field(name="Successfully connected to voice channel:", value=channel)
+    await client.say(embed=embed)
 
-@client.command(pass_context=True, no_pm=True)
-async def leave(ctx):
+@client.command(name="leave", pass_context=True, no_pm=True)
+async def _leave(ctx):
+    user = ctx.message.author
     server = ctx.message.server
     channel = ctx.message.author.voice.voice_channel
     voice_client = client.voice_client_in(server)
     await voice_client.disconnect()
-    await client.say("Successfully disconnected from ***[{}]***".format(channel))
+    embed = discord.Embed(colour=user.colour)
+    embed.add_field(name="Successfully disconnected from:", value=channel)
+    await client.say(embed=embed)
+
+@client.command(pass_context=True)
+async def pause(ctx):
+    user = ctx.message.author
+    id = ctx.message.server.id
+    players[id].pause()
+    embed = discord.Embed(colour=user.colour)
+    embed.add_field(name="Player Paused", value=f"Requested by {ctx.message.author.name}")
+    await client.say(embed=embed)
+
+@client.command(pass_context=True)
+async def skip(ctx):
+    user = ctx.message.author
+    id = ctx.message.server.id
+    players[id].stop()
+    embed = discord.Embed(colour=user.colour)
+    embed.add_field(name="Player Skipped", value=f"Requested by {ctx.message.author.name}")
+    await client.say(embed=embed)
 
 @client.command(name="play", pass_context=True)
 async def _play(ctx, *, name):
 	author = ctx.message.author
-	name = ctx.message.content.replace("xplay ", '')
+	name = ctx.message.content.replace("m.play ", '')
 	fullcontent = ('http://www.youtube.com/results?search_query=' + name)
 	text = requests.get(fullcontent).text
 	soup = bs4.BeautifulSoup(text, 'html.parser')
@@ -76,7 +90,7 @@ async def _play(ctx, *, name):
 	url = ('http://www.youtube.com'+a0['href'])
 	server = ctx.message.server
 	voice_client = client.voice_client_in(server)
-	player = await voice_client.create_ytdl_player(url)
+	player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
 	players[server.id] = player
 	print("User: {} From Server: {} is playing {}".format(author, server, title))
 	player.start()
@@ -85,25 +99,37 @@ async def _play(ctx, *, name):
 	await client.say(embed=embed)
 
 @client.command(pass_context=True)
-async def pause(ctx):
-    id = ctx.message.server.id
-    players[id].pause()
-    await client.say("Player Paused")
+async def queue(ctx, *, name):
+	name = ctx.message.content.replace("m.queue ", '')
+	fullcontent = ('http://www.youtube.com/results?search_query=' + name)
+	text = requests.get(fullcontent).text
+	soup = bs4.BeautifulSoup(text, 'html.parser')
+	img = soup.find_all('img')
+	div = [ d for d in soup.find_all('div') if d.has_attr('class') and 'yt-lockup-dismissable' in d['class']]
+	a = [ x for x in div[0].find_all('a') if x.has_attr('title') ]
+	title = (a[0]['title'])
+	a0 = [ x for x in div[0].find_all('a') if x.has_attr('title') ][0]
+	url = ('http://www.youtube.com'+a0['href'])
+	server = ctx.message.server
+	voice_client = client.voice_client_in(server)
+	player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
 
-@client.command(pass_context=True)
-async def stop(ctx):
-    author = ctx.message.author
-    id = ctx.message.server.id
-    players[id].stop()
-    embed = discord.Embed(description=" ")
-    embed.add_field(name="Player Stopped", value=f"Requested by {ctx.message.author.name}")
-    await client.say(embed=embed)
+	if server.id in queues:
+		queues[server.id].append(player)
+	else:
+		queues[server.id] = [player]
+	embed = discord.Embed(description=" ")
+	embed.add_field(name="Video queued", value=title)
+	await client.say(embed=embed)
 
 @client.command(pass_context=True)
 async def resume(ctx):
+    user = ctx.message.author
     id = ctx.message.server.id
     players[id].resume()
-    await client.say("player resumed")
+    embed = discord.Embed(colour=user.colour)
+    embed.add_field(name="Player Resumed", value=f"Requested by {ctx.message.author.name}")
+    await client.say(embed=embed)
 
 @client.command(pass_context=True, no_pm=True)
 async def help(ctx):
@@ -115,5 +141,24 @@ async def help(ctx):
 	embed.add_field(name="xping", value="get bot's ping time")
 	embed.add_filed(name="Comming Soon", value="New Loop Command & Queue Commands Comming Soon")
 	await client.say(embed=embed)
+	
+def user_is_me(ctx):
+	return ctx.message.author.id == "381562121865003009"
 
+@client.command(name='eval', pass_context=True)
+@commands.check(user_is_me)
+async def _eval(ctx, *, command):
+    res = eval(command)
+    if inspect.isawaitable(res):
+        await client.say(await res)
+    else:
+    	await client.delete_message(ctx.message)
+    	await client.say(res)
+        
+@_eval.error
+async def eval_error(error, ctx):
+	if isinstance(error, discord.ext.commands.errors.CheckFailure):
+		text = "Sorry {}, You can't use this command only the bot owner can do this.".format(ctx.message.author.mention)
+		await client.send_message(ctx.message.channel, text)
+	
 client.run(os.environ['BOT_TOKEN'])
