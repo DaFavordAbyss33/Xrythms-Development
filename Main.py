2939558e81124bde9d99f2f3335c3536392edbbc
noobpersonTOs <46160387+noobpersonTOs@users.noreply.github.com>
2019-02-17 14:07:47 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,11 +3,36 @@ from discord.ext import commands
 import inspect
 import os
 import asyncio
+import requests, bs4
+import youtube_dl
+import datetime
+from discord import opus
+
+start_time = time.time()
 
 @bot.event
 async def on_ready():
   print("Logged in as")
   print("bot user Name:", bot.user.name)
   print("bot user ID:", bot.user.id)
+  
+def user_is_me(ctx):
+	return ctx.message.author.id == "277983178914922497"
+
+@bot.command(name='eval', pass_context=True)
+@commands.check(user_is_me)
+async def _eval(ctx, *, command):
+    res = eval(command)
+    if inspect.isawaitable(res):
+        await bot.say(await res)
+    else:
+    	await bot.delete_message(ctx.message)
+    	await bot.say(res)
+        
+@_eval.error
+async def eval_error(error, ctx):
+	if isinstance(error, discord.ext.commands.errors.CheckFailure):
+		text = "Sorry {}, You can't use this command only the bot owner can do this.".format(ctx.message.author.mention)
+		await bot.send_message(ctx.message.channel, text)
   
 bot.run(os.environ['BOT_TOKEN'])
