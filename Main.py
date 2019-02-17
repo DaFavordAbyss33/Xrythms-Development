45de4168485e7f00400084032d0b45d4d7dc5ad2
noobpersonTOs <46160387+noobpersonTOs@users.noreply.github.com>
2019-02-17 13:57:29 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,12 @@
+import discord
+from discord.ext import commands
+import inspect
+import os
 
+@bot.event
+async def on_ready():
+  print("Logged in as")
+  print("bot user Name:", bot.user.name)
+  print("bot user ID:", bot.user.id)
+  
+bot.run(os.environ['BOT_TOKEN'])
